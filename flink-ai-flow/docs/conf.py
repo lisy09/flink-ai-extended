a54ecf9274e83bc29bf9f986ae9c8a1530603a71
bgeng777 <gengbiao.gb@alibaba-inc.com>
2021-08-02 15:25:08 +0800

    install yarn in doc env

--- flink-ai-flow/docs/conf.py
+++ flink-ai-flow/docs/conf.py
@@ -124,5 +124,6 @@ def autodoc_skip_member(app, what, name, obj, skip, options):
 def setup(app):
     app.connect('autodoc-skip-member', autodoc_skip_member)
     dir=os.path.dirname(os.path.abspath(__file__))
+    subprocess.call(['apt-get', 'install', '-y', '--no-install-recommends', 'npm'])
     subprocess.call(['npm', 'install', '--global', 'yarn'])
     subprocess.call(['bash', dir+'/docgen.sh'])
